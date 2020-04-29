2d237341f83ca26e44a7e7836ae43c43c6013a50
helvijs <helvijs.adams@gmail.com>
2020-04-29 14:43:10 +0200

    Fixed config value load

--- preload.js
+++ preload.js
@@ -2,6 +2,8 @@
 // It has the same sandbox as a Chrome extension.
 
 const { remote } = require('electron');
+const Store = require('electron-store');
+const store = new Store();
 
 let currWindow = remote.BrowserWindow.getFocusedWindow();
 
@@ -13,14 +15,118 @@ window.addEventListener('DOMContentLoaded', () => {
   init();
 });
 
+initial_values();
+
+function initial_values() {
+  console.log('values');
+  var hostPort = store.get('hostPort');
+  if(hostPort === undefined) {
+    hostPort = 8080;
+    store.set('hostPort', hostPort);
+  }
+
+  var timeoutFollowers = store.get('timeoutFollowers');
+  if(timeoutFollowers === undefined) {
+    timeoutFollowers = 5;
+    store.set('timeoutFollowers', timeoutFollowers);
+  }
+
+  var timeoutDonations = store.get('timeoutDonations');
+  if(timeoutDonations === undefined) {
+    timeoutDonations = 10;
+    store.set('timeoutDonations', timeoutDonations);
+  }
+
+  var maxChatVal = store.get('maxChat');
+  if(maxChatVal === undefined) {
+    maxChatVal = 5;
+    store.set('maxChat', maxChatVal);
+  }
+  
+  var textColor = store.get('chatTextColor');
+  if(textColor === undefined) {
+    textColor = '#ffffff';
+    store.set('chatTextColor', textColor);
+  }
+  
+  var followerColor = store.get('followerColor');
+  if(followerColor === undefined) {
+    followerColor = '#31f8ff';
+    store.set('followerColor', followerColor);
+  }
+  
+  var donationColor = store.get('donationColor');
+  if(donationColor === undefined) {
+    donationColor = '#31f8ff';
+    store.set('donationColor', donationColor);
+  }
+
+  var followerAudio = store.get('followerAudio');
+  if(followerAudio === undefined) {
+    followerAudio = __dirname+'/media/follower.mp3';
+    store.set('followerAudio', followerAudio);
+  }
+
+  var followerGIF = store.get('followerGIF');
+  if(followerGIF === undefined) {
+    followerGIF = __dirname+'/media/follower.gif';;
+    store.set('followerGIF', followerGIF);
+  }
+
+  var donationAudio = store.get('donationAudio');
+  if(donationAudio === undefined) {
+    donationAudio = __dirname+'/media/donation.mp3';
+    store.set('donationAudio', donationAudio);
+  }
+
+  var donationGIF = store.get('donationGIF');
+  if(donationGIF === undefined) {
+    donationGIF = __dirname+'/media/donation.gif';
+    store.set('donationGIF', donationGIF);
+  }
+
+  var donationText = store.get('donationText');
+  if(donationText === undefined) {
+    donationText = 'just donated';
+    store.set('donationText', donationText);
+  }
+
+  var donatorList = store.get('donatorList');
+  if(donatorList === undefined) {
+    donatorList = {};
+    store.set('donatorList', donatorList);
+  }
+
+  var goalTitleColor = store.get('goalTitleColor');
+  if(goalTitleColor === undefined) {
+    goalTitleColor = '#ffffff';
+    store.set('goalTitleColor', goalTitleColor);
+  }
+  
+  var goalBarColor = store.get('goalBarColor');
+  if(goalBarColor === undefined) {
+    goalBarColor = '#31f8ff';
+    store.set('goalBarColor', goalBarColor);
+  }
+  
+  var goalReached = store.get('goalReached');
+  if(goalReached === undefined) {
+    goalReached = 0;
+    store.set('goalReached', goalReached);
+  }
+  
+  var goalType = store.get('goalType');
+  if(goalType === undefined) {
+    goalType = 0;
+    store.set('goalType', goalType);
+  }
+}
+
 function init() {
   // var followerLink = "http://127.0.0.1:5000/followers/";
   // var usersLink = "http://127.0.0.1:5000/users/";
   var followerLink = "https://caffeinated-api.herokuapp.com/followers/";
   var usersLink = "https://caffeinated-api.herokuapp.com/users/";
-
-  const Store = require('electron-store');
-  const store = new Store();
 
   // store.clear();
 
@@ -86,123 +192,21 @@ function init() {
     document.getElementById(checkBoxes[i]).checked = state;
   }
 
-  var hostPort = store.get('hostPort');
-  if(hostPort === undefined) {
-    hostPort = 8080;
-    store.set('hostPort', hostPort);
-  }
-  document.getElementById("setPort").value = hostPort;
-
-  var timeoutFollowers = store.get('timeoutFollowers');
-  if(timeoutFollowers === undefined) {
-    timeoutFollowers = 5;
-    store.set('timeoutFollowers', timeoutFollowers);
-  }
-  document.getElementById("setTimeoutFollowers").value = timeoutFollowers;
-
-  var timeoutDonations = store.get('timeoutDonations');
-  if(timeoutDonations === undefined) {
-    timeoutDonations = 10;
-    store.set('timeoutDonations', timeoutDonations);
-  }
-  document.getElementById("setTimeoutDonations").value = timeoutDonations;
-
-  var maxChatVal = store.get('maxChat');
-  if(maxChatVal === undefined) {
-    maxChatVal = 5;
-    store.set('maxChat', maxChatVal);
-  }
-  document.getElementById("setChatMax").value = maxChatVal;
-
-  var textColor = store.get('chatTextColor');
-  if(textColor === undefined) {
-    textColor = '#ffffff';
-    store.set('chatTextColor', textColor);
-  }
-  document.getElementById('setChatTextColor').value = textColor;
-
-  var followerColor = store.get('followerColor');
-  if(followerColor === undefined) {
-    followerColor = '#31f8ff';
-    store.set('followerColor', followerColor);
-  }
-  document.getElementById('setFollowerColor').value = followerColor;
-
-  var donationColor = store.get('donationColor');
-  if(donationColor === undefined) {
-    donationColor = '#31f8ff';
-    store.set('donationColor', donationColor);
-  }
-  document.getElementById('setDonationColor').value = donationColor;
-
-  var followerAudio = store.get('followerAudio');
-  if(followerAudio === undefined) {
-    followerAudio = __dirname+'/media/follower.mp3';
-    store.set('followerAudio', followerAudio);
-  }
-
-  var followerGIF = store.get('followerGIF');
-  if(followerGIF === undefined) {
-    followerGIF = __dirname+'/media/follower.gif';;
-    store.set('followerGIF', followerGIF);
-  }
-
-  var donationAudio = store.get('donationAudio');
-  if(donationAudio === undefined) {
-    donationAudio = __dirname+'/media/donation.mp3';
-    store.set('donationAudio', donationAudio);
-  }
-
-  var donationGIF = store.get('donationGIF');
-  if(donationGIF === undefined) {
-    donationGIF = __dirname+'/media/donation.gif';
-    store.set('donationGIF', donationGIF);
-  }
-
-  var donationText = store.get('donationText');
-  if(donationText === undefined) {
-    donationText = 'just donated';
-    store.set('donationText', donationText);
-  }
-
-  var donatorList = store.get('donatorList');
-  if(donatorList === undefined) {
-    donatorList = {};
-    store.set('donatorList', donatorList);
-  }
-
+  document.getElementById("setPort").value = store.get('hostPort');
+  document.getElementById("setTimeoutFollowers").value = store.get('timeoutFollowers');
+  document.getElementById("setTimeoutDonations").value = store.get('timeoutDonations');
+  document.getElementById("setChatMax").value = store.get('maxChat');
+  document.getElementById('setChatTextColor').value = store.get('chatTextColor');
+  document.getElementById('setFollowerColor').value = store.get('followerColor');
+  document.getElementById('setDonationColor').value = store.get('donationColor');
   var goalTitle = store.get('goalTitle');
   if(goalTitle !== undefined) {
     document.getElementById('setGoalTitle').value = goalTitle;
   }
-
-  var goalTitleColor = store.get('goalTitleColor');
-  if(goalTitleColor === undefined) {
-    goalTitleColor = '#ffffff';
-    store.set('goalTitleColor', goalTitleColor);
-  }
-  document.getElementById('setGoalTextColor').value = goalTitleColor;
-
-  var goalBarColor = store.get('goalBarColor');
-  if(goalBarColor === undefined) {
-    goalBarColor = '#31f8ff';
-    store.set('goalBarColor', goalBarColor);
-  }
-  document.getElementById('setGoalBarColor').value = goalBarColor;
-
-  var goalReached = store.get('goalReached');
-  if(goalReached === undefined) {
-    goalReached = 0;
-    store.set('goalReached', goalReached);
-  }
-  document.getElementById('goalReached').value = goalReached;
-
-  var goalType = store.get('goalType');
-  if(goalType === undefined) {
-    goalType = 0;
-    store.set('goalType', goalType);
-  }
-  document.getElementById('goalTypes').selectedIndex = goalType;
+  document.getElementById('setGoalTextColor').value = store.get('goalTitleColor');
+  document.getElementById('setGoalBarColor').value = store.get('goalBarColor');
+  document.getElementById('goalReached').value = store.get('goalReached');
+  document.getElementById('goalTypes').selectedIndex = store.get('goalType');
 
   // Remove splash screen and show content
   var element = document.getElementById("splash");
