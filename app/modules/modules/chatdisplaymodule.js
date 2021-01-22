1309c3f01a50a8555fce72a08863c3221efc768c
Ender <33337309+e3ndr@users.noreply.github.com>
2021-01-22 04:35:23 -0600

    1.0-stable5
    Redo the chat window

--- app/modules/modules/chatdisplaymodule.js
+++ app/modules/modules/chatdisplaymodule.js
@@ -8,6 +8,8 @@ MODULES.moduleClasses["casterlabs_chat_display"] = class {
         this.id = id;
         this.icon = "chatbox";
 
+        this.messageHistory = [];
+
         // Where the magic happens
         this.pageSrc = __dirname + "/modules/modules/chatdisplay.html";
 
@@ -54,12 +56,20 @@ MODULES.moduleClasses["casterlabs_chat_display"] = class {
 
         /* Listeners */
 
-        this.contentDocument.querySelector("#vcopen").addEventListener("click", () => {
-            this.createWindow();
+        this.contentDocument.addEventListener("upvote_request", (e) => {
+            koi.upvote(e.detail.id);
         });
 
-        this.contentDocument.querySelector("#vcjumpdown").addEventListener("click", () => {
-            this.jumpDown();
+        this.contentDocument.addEventListener("open_link", (e) => {
+            openLink(e.detail.link);
+        });
+
+        this.contentDocument.querySelector("#vcpopoutviewers").addEventListener("click", () => {
+            this.createViewersWindow();
+        });
+
+        this.contentDocument.querySelector("#vcpopout").addEventListener("click", () => {
+            this.createPopoutWindow();
         });
 
         const messageInput = this.contentDocument.querySelector("#vcmessage");
@@ -75,20 +85,9 @@ MODULES.moduleClasses["casterlabs_chat_display"] = class {
             koi.sendMessage(messageInput.value);
             messageInput.value = "";
         });
-
-        this.contentDocument.querySelector("#chatbox").addEventListener("scroll", () => {
-            this.checkJumpButton();
-        });
-
     }
 
-    updateViewers() {
-        if (this.viewersWindow) {
-            this.viewersWindow.webContents.executeJavaScript("setViewers(" + JSON.stringify(this.viewersList) + ");");
-        }
-    }
-
-    createWindow() {
+    createViewersWindow() {
         if (!this.viewersWindow) {
             const mainWindowState = windowStateKeeper({
                 defaultWidth: 200,
@@ -114,7 +113,10 @@ MODULES.moduleClasses["casterlabs_chat_display"] = class {
                 }
             });
 
+            this.contentDocument.querySelector("#vcpopoutviewers").classList.add("hide");
+
             this.viewersWindow.once("close", () => {
+                this.contentDocument.querySelector("#vcpopoutviewers").classList.remove("hide");
                 this.viewersWindow = null;
             });
 
@@ -130,197 +132,111 @@ MODULES.moduleClasses["casterlabs_chat_display"] = class {
         }
     }
 
-    messageUpvote(event) {
-        let element = this.contentDocument.querySelector("[vc_message_id='" + event.id + "']");
-
-        // Sometimes messages don't exist.
-        if (element) {
-            element.innerText = event.upvotes;
-
-            if (event.upvotes >= 1000) {
-                element.classList = "upvote-4";
-            } else if (event.upvotes >= 100) {
-                element.classList = "upvote-3";
-            } else if (event.upvotes >= 10) {
-                element.classList = "upvote-2";
-            } else if (event.upvotes >= 1) {
-                element.classList = "upvote-1";
-            }
-        }
-    }
-
-    addMessage(event) {
-        const div = this.contentDocument.createElement("div");
-        const username = this.contentDocument.createElement("span");
-        const pfp = this.contentDocument.createElement("img");
-        const text = this.contentDocument.createElement("span");
-
-        const msg = this.contentDocument.createElement("li");
-        const tooltip = this.contentDocument.createElement("ul");
-        const counter = this.contentDocument.createElement("sup");
-
-        pfp.src = event.sender.image_link;
-        pfp.classList.add("vcprofile");
-        pfp.addEventListener("click", () => {
-            openLink(event.sender.link);
-        });
-
-        username.classList.add("vcusername");
-        username.style = "color: " + event.sender.color + ";";
-        username.appendChild(pfp);
-        username.appendChild(this.contentDocument.createTextNode(event.sender.username));
-
-        event.sender.badges.forEach((badge) => {
-            const badgeIcon = this.contentDocument.createElement("img");
-
-            badgeIcon.src = badge;
-            badgeIcon.classList = "vcbadge";
-
-            username.appendChild(badgeIcon);
-        });
-
-        username.appendChild(text);
-
-        text.classList.add("vctext");
-
-        event.message = escapeHtml(event.message);
-
-        for (const [name, link] of Object.entries(event.emotes)) {
-            event.message = event.message.split(name).join(`<img class="vcimage" title="${name}" src="${link}" />`);
-        }
-
-        text.innerHTML = event.message;
-
-        counter.setAttribute("vc_message_id", event.id);
-
-        div.classList.add("vcchatmessage");
-        div.appendChild(username);
-
-        div.appendChild(counter);
-
-        if (event.donations) {
-            if (isPlatform("CAFFEINE")) { // Caffeine has cute little prop images, so we display those instead of just the amount
-                event.donations.forEach((donation) => {
-                    const img = this.contentDocument.createElement("img");
-
-                    img.src = donation.image;
-                    img.classList = "vcimage";
-
-                    username.appendChild(img);
-                });
-            } else {
-                event.donations.forEach((donation) => {
-                    const donationContainer = this.contentDocument.createElement("span");
-
-                    donationContainer.innerHTML = formatCurrency(donation.amount, donation.currency);
-
-                    username.appendChild(donationContainer);
-                });
-            }
-        }
-
-        msg.appendChild(div);
-
-        if (isPlatform("CAFFEINE")) {
-            if (event.id !== "") {
-                const upvotetooltip = this.contentDocument.createElement("li");
-                const upvotebtn = this.contentDocument.createElement("a");
-
-                upvotebtn.innerHTML = `<ion-icon name="arrow-up"></ion-icon>`;
-                upvotebtn.title = "Upvote";
-                upvotebtn.addEventListener("click", () => {
-                    koi.upvote(event.id);
-                    upvotetooltip.remove();
-                });
-
-                upvotetooltip.appendChild(upvotebtn);
-                upvotetooltip.classList.add("tooltipbtn");
-                tooltip.appendChild(upvotetooltip);
-            }
-        }
-
-        tooltip.classList.add("tip");
-        msg.appendChild(tooltip);
-
-        this.contentDocument.querySelector("#chatbox").appendChild(msg);
-
-        this.tryJump();
-    }
-
-
-    addStatus(user, profilePic, color, type) {
-        const div = this.contentDocument.createElement("div");
-        const pfp = this.contentDocument.createElement("img");
-        const username = this.contentDocument.createElement("span");
-        const text = this.contentDocument.createElement("span");
-        const msg = this.contentDocument.createElement("li");
-
-        pfp.src = profilePic;
-        pfp.classList.add("vcprofile");
-
-        username.classList.add("vcusername");
-        username.style = "color: " + color + ";";
-        username.innerText = user;
-
-        switch (type.toLowerCase()) {
-            case "leave": text.innerText = "left the stream."; break;
-            case "join": text.innerText = "joined the stream."; break;
-            case "follow": text.innerText = "started following."; break;
-        }
-
-        div.classList.add("vcchatmessage");
-        div.classList.add("vcstatus");
-        div.appendChild(pfp);
-        div.appendChild(username);
-        div.appendChild(text);
-
-        msg.appendChild(div);
-
-        this.contentDocument.querySelector("#chatbox").appendChild(msg);
-
-        this.tryJump();
-    }
-
-    isAtBottom() {
-        const scrollOffset = this.contentDocument.querySelector("#chatbox").scrollHeight - this.contentDocument.querySelector("#chatbox").scrollTop;
-        const height = this.contentDocument.querySelector("#chatbox").offsetHeight;
-
-        return (scrollOffset - height) < 100;
-    }
-
-    tryJump() {
-        this.checkJumpButton();
-
-        if (this.isAtBottom()) {
-            this.jumpDown();
-        }
-    }
-
-    checkJumpButton() {
-        const jump = this.contentDocument.querySelector("#vcjumpdown");
-
-        if (this.isAtBottom()) {
-            anime({
-                targets: jump,
-                easing: "linear",
-                opacity: 0,
-                duration: 100
-            }).finished.then(() => {
-                jump.classList.add("hide");
+    createPopoutWindow() {
+        if (!this.popoutWindow) {
+            const mainWindowState = windowStateKeeper({
+                defaultWidth: 300,
+                defaultHeight: 500,
+                file: "caffeinated-chat-popout-window.json"
             });
-        } else {
-            jump.classList.remove("hide");
-            anime({
-                targets: jump,
-                easing: "linear",
-                opacity: 1,
-                duration: 100
+
+            this.popoutWindow = new BrowserWindow({
+                minWidth: 300,
+                minHeight: 500,
+                width: mainWindowState.width,
+                height: mainWindowState.height,
+                x: mainWindowState.x,
+                y: mainWindowState.y,
+                resizable: true,
+                transparent: false,
+                show: false,
+                titleBarStyle: "shown",
+                icon: __dirname + "/media/app_icon.png",
+                frame: false,
+                webPreferences: {
+                    nodeIntegration: true
+                }
             });
+
+            this.contentDocument.querySelector("#vcpopout").classList.add("hide");
+
+            this.popoutWindow.once("close", () => {
+                this.contentDocument.querySelector("#vcpopout").classList.remove("hide");
+                this.popoutWindow = null;
+            });
+
+            this.popoutWindow.once("ready-to-show", () => {
+                this.popoutWindow.show();
+
+                this.popoutWindow.webContents.executeJavaScript(`catchUp(${JSON.stringify(this.messageHistory)})`);
+            });
+
+            this.popoutWindow.addListener("upvote_request", (id) => {
+                koi.upvote(id);
+            });
+
+            this.popoutWindow.addListener("send_message", (message) => {
+                koi.sendMessage(message);
+            });
+
+            this.popoutWindow.loadFile(__dirname + "/modules/modules/chatdisplay.html");
+
+            mainWindowState.manage(this.popoutWindow);
         }
     }
 
-    jumpDown() {
-        this.contentDocument.querySelector("#chatbox").scrollTo(0, this.contentDocument.querySelector("#chatbox").scrollHeight + 1000);
+    updateViewers() {
+        if (this.viewersWindow) {
+            this.viewersWindow.webContents.executeJavaScript(`setViewers(${JSON.stringify(this.viewersList)})`);
+        }
+    }
+
+    messageUpvote(event, onlyPopout) {
+        if (this.popoutWindow) {
+            this.popoutWindow.webContents.executeJavaScript(`messageUpvote(${JSON.stringify(event)})`);
+        }
+
+        if (!onlyPopout) {
+            this.messageHistory.push({
+                type: "UPVOTE",
+                event: event
+            });
+
+            this.contentWindow.messageUpvote(event);
+        }
+    }
+
+    addMessage(event, onlyPopout) {
+        if (this.popoutWindow) {
+            this.popoutWindow.webContents.executeJavaScript(`addMessage(${JSON.stringify(event)})`);
+        }
+
+        if (!onlyPopout) {
+            this.messageHistory.push({
+                type: "MESSAGE",
+                event: event
+            });
+
+            this.contentWindow.addMessage(event);
+        }
+    }
+
+    addStatus(user, profilePic, color, type, onlyPopout) {
+        if (this.popoutWindow) {
+            this.popoutWindow.webContents.executeJavaScript(`addStatus(${JSON.stringify(user)}, ${JSON.stringify(profilePic)}, ${JSON.stringify(color)}, ${JSON.stringify(type)})`);
+        }
+
+        if (!onlyPopout) {
+            this.messageHistory.push({
+                type: "STATUS",
+                user: user,
+                profilePic: profilePic,
+                color: color,
+                statusType: type
+            });
+
+            this.contentWindow.addStatus(user, profilePic, color, type);
+        }
     }
 
 };
