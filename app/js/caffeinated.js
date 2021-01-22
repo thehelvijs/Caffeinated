73fea5b2b9e38b7dd62ce73011df8c6889262961
Ender <33337309+e3ndr@users.noreply.github.com>
2021-01-22 04:03:47 -0600

    Fix issue with things disappearing the user bar

--- app/js/caffeinated.js
+++ app/js/caffeinated.js
@@ -7,8 +7,8 @@ const { ipcRenderer } = require("electron");
 const { app, ipcMain, BrowserWindow, globalShortcut } = require("electron").remote;
 const windowStateKeeper = require("electron-window-state");
 
-const PROTOCOLVERSION = 27;
-const VERSION = "1.0-stable4";
+const PROTOCOLVERSION = 28;
+const VERSION = "1.0-stable5";
 
 const koi = new Koi("wss://api.casterlabs.co/v2/koi");
 
