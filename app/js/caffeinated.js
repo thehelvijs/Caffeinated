0e42ed56a462c7783b8ae9d78064de48e8c76136
Ender <33337309+e3ndr@users.noreply.github.com>
2021-02-02 18:50:28 -0600

    1.1-stable3

--- app/js/caffeinated.js
+++ app/js/caffeinated.js
@@ -7,8 +7,8 @@ const { ipcRenderer } = require("electron");
 const { app, ipcMain, BrowserWindow, globalShortcut } = require("electron").remote;
 const windowStateKeeper = require("electron-window-state");
 
-const PROTOCOLVERSION = 42;
-const VERSION = "1.1-stable2";
+const PROTOCOLVERSION = 43;
+const VERSION = "1.1-stable3";
 
 const LOGIN_BUTTONS = {
     STABLE: `
