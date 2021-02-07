71725db078debc5356c2880010deeffa2bcd5edd
Ender <33337309+e3ndr@users.noreply.github.com>
2021-02-06 20:54:54 -0600

    Make the supporters list prettier.

--- app/modules/modules/supporters.js
+++ app/modules/modules/supporters.js
@@ -13,16 +13,19 @@ MODULES.moduleClasses["casterlabs_supporters"] = class {
     init() {
         this.page.innerHTML = `
             <br />
-            <p style="text-align:center">
-                Loving Caffeinated? Feel free to support the project <a style="color:#e94b4b" onclick="openLink('https://ko-fi.com/casterlabs')">here</a>.
+            <p style="text-align: center;">
+                Loving Caffeinated? Feel free to support the project <a style="color: #e94b4b;" onclick="openLink('https://ko-fi.com/casterlabs')">here</a>.
             </p>
-            <p style="text-align:center">
-                Looking for a custom made overlay design?<br />Get your own at <a style="color:#ff94db" onclick="openLink('https://reyana.org')">Reyana.org <img style="height: .85em; vertical-align: middle;" src="https://assets.casterlabs.co/butterfly.png" /></a>
+            <p style="text-align: center;">
+                Looking for a custom made overlay design?<br />Get your own at <a style="color: #ff94db;" onclick="openLink('https://reyana.org')">Reyana.org <img style="height: .85em; vertical-align: middle;" src="https://assets.casterlabs.co/butterfly.png" /></a>
             </p>
-            <h5 style="text-align:center">
+            <br />
+            <h5 style="text-align: center;">
                 ★ Our Supporters ★
             </h5>
-            <div style="text-align:center;max-height:100%;overflow:auto;" id="supporters"></div>
+            <div style="text-align: center;" id="supporters"></div>
+            <br />
+            <br />
             <br />
         `;
 
@@ -32,7 +35,7 @@ MODULES.moduleClasses["casterlabs_supporters"] = class {
     }
 
     update() {
-        fetch("https://caffeinated.casterlabs.co/supporters.json").then((response) => response.json()).then((donations) => {
+        fetch("https://api.casterlabs.co/v1/caffeinated/supporters").then((response) => response.json()).then((donations) => {
             const div = document.querySelector("#supporters");
 
             div.innerHTML = "";
