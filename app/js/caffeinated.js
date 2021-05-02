d8580bb50bcd827f7e5e1b3f6999b263d05f98fa
Ender <33337309+e3ndr@users.noreply.github.com>
2021-05-02 05:03:49 -0500

    1.1-stable32

--- app/js/caffeinated.js
+++ app/js/caffeinated.js
@@ -8,8 +8,8 @@ const { app, ipcMain, BrowserWindow, globalShortcut } = require("electron").remo
 const windowStateKeeper = require("electron-window-state");
 const RPC = require("discord-rpc");
 
-const PROTOCOLVERSION = 71;
-const VERSION = "1.1-stable31";
+const PROTOCOLVERSION = 72;
+const VERSION = "1.1-stable32";
 const CLIENT_ID = "LmHG2ux992BxqQ7w9RJrfhkW";
 const BROWSERWINDOW = electron.getCurrentWindow();
 
