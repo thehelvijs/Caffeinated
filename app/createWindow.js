8a6e8cdf14254df8ccc895d4af4dc7146bd3a014
Ender <33337309+e3ndr@users.noreply.github.com>
2021-07-18 19:38:07 -0500

    Make createWindow.js, for the new launcher. (1.1-stable38)

---
+++ app/createWindow.js
@@ -0,0 +1,51 @@
+const { BrowserWindow } = require("electron");
+const windowStateKeeper = require("electron-window-state");
+
+function createWindow() {
+    const mainWindowState = windowStateKeeper({
+        defaultWidth: 700,
+        defaultHeight: 500,
+        file: "main-window.json"
+    });
+
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        minWidth: 700,
+        minHeight: 500,
+        width: mainWindowState.width,
+        height: mainWindowState.height,
+        x: mainWindowState.x,
+        y: mainWindowState.y,
+        transparent: false,
+        resizable: true,
+        show: false,
+        backgroundColor: "#141414",
+        icon: __dirname + "/media/app_icon.png",
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            webSecurity: false
+        }
+    })
+
+    // and load the index.html of the app.
+    mainWindow.loadFile("index.html");
+    mainWindowState.manage(mainWindow);
+
+    // Emitted when the window is closed.
+    mainWindow.on("closed", () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+
+    // Emitted when the window is ready to be shown
+    // This helps in showing the window gracefully.
+    mainWindow.once("ready-to-show", () => {
+        mainWindow.show();
+    });
+}
+
+module.exports = createWindow;
--- app/js/caffeinated.js
+++ app/js/caffeinated.js
@@ -8,8 +8,8 @@ const { app, ipcMain, BrowserWindow, globalShortcut } = require("electron").remo
 const windowStateKeeper = require("electron-window-state");
 const RPC = require("discord-rpc");
 
-const PROTOCOLVERSION = 79;
-const VERSION = "1.1-stable37";
+const PROTOCOLVERSION = 78;
+const VERSION = "1.1-stable38";
 const CLIENT_ID = "LmHG2ux992BxqQ7w9RJrfhkW";
 const BROWSERWINDOW = electron.getCurrentWindow();
 
